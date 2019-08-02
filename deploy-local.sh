ac17aa2a9fffdf7c63475f53eb8837a240f19d67
Dan Patiu <dan.patiu@pitechnologies.ro>
2019-08-02 15:22:16 +0300

    fix: build and deploy scripts and select name

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,4 +1,6 @@
-npm run build
+rm -rf ./dist
+ng build
+npm pack ./dist/ngrx-hocus
 PACKAGE=$(npm pack ./dist/ngrx-hocus)
 mv ${PACKAGE} ../ngrx-hocus-app
 cd ../ngrx-hocus-app && npm install $PACKAGE --save
--- projects/ngrx-hocus/src/lib/select.ts
+++ projects/ngrx-hocus/src/lib/select.ts
@@ -10,7 +10,7 @@ export type SelectFrom<State> =
   (<a extends keyof State, b extends keyof State[a], c extends keyof State[a][b], d extends keyof State[a][b][c], e extends keyof State[a][b][c][d]>(key1: a, key2: b, key3: c, key4: d, key5: e) => (target: any, propertyName: string | symbol) => void) &
   (<a extends keyof State, b extends keyof State[a], c extends keyof State[a][b], d extends keyof State[a][b][c], e extends keyof State[a][b][c][d], f extends keyof State[a][b][c][d][e]>(key1: a, key2: b, key3: c, key4: d, key5: e, key6: f) => (target: any, propertyName: string | symbol) => void);
 
-export function SelectDecorator(...args) {
+export function Select(...args) {
   return (target: any, propertyName: string | symbol) => {
     const metadata = new SelectMetadata(propertyName, args);
 

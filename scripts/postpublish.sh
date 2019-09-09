0af12359b09cae74bd93b4ba8035575bc377188f
Dan Patiu <dan.patiu@nordlogic.com>
2019-09-09 22:08:52 +0300

    ci: potential fix for ci push tags

--- scripts/postpublish.sh
+++ scripts/postpublish.sh
@@ -3,6 +3,9 @@ version=$(node -p -e "require('./dist/ngrx-hocus/package.json').version")
 
 echo "Tagging '$version'"
 
+git config --global user.email dan.patiu@gmail.com
+git config --global user.name lolismatic
+
 git tag -a "$version" -m "bump ci"
 git push origin $version -f
 
